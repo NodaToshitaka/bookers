41f0931379de0281d6967baf9178e0f1016886a3
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2021-12-05 21:31:27 +0000

    make all file. and doing css now

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,45 @@ class BooksController < ApplicationController
   end
 
   def show
-  end
+    @book = Book.find(params[:id])
 
-  def new
+  end
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+
   end
 
   def edit
+    @book = Book.find(params[:id])
+
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
